c2d8427ddd1d4a6f139a5e3f7dba5e64238453c8
Joseph Neff <Jneff140@gmail.com>
2022-01-10 17:59:13 -0500

    PyBank Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -20,32 +20,29 @@ with open(csvpath) as csvfile:
     for i in range(1, len(profits)):
         change_in_profit.append((int(profits[i]) - int(profits[i-1])))
 
-    
-    
-    #for profit in profits:
-        #change_in_profit.append(profits[profit+1] - profits[profit])
-        #change_in_profit.append(profits[profit+1] - profits[profit])
-        #profits = [int(i) for i in profits]
-        #change_in_profit.append = profits[i+1] - profits[i]
-        #current_profit
-        #profit_change.append(int(current_profit) - int(previous_profit))
-        #previous_profit = 
-   
-
-
-#print(profits)        
 month_count = len(months)
 profits_total = sum(profits)
 average_profit = int(sum(change_in_profit))/int(len(change_in_profit))
 max_change = max(change_in_profit)
 min_change = min(change_in_profit)
 
-
 print("Financial Analysis")
 print("-------------------")
 print('Total Months: ' + str(month_count))
 print('Total Profit: ' + "$" + str(profits_total))
 print('Average Change: ' "$" + str(round(average_profit)))
-print(min_change)
-print(max_change)
+print("Greatest Profit Increase: " + str(months[change_in_profit.index(max(change_in_profit))+1]) + " " + "$" + str(max_change))
+print("Greatest Profit Decrease: " + str(months[change_in_profit.index(min(change_in_profit))+1]) + " " + "$" + str(min_change))
 
+output_path = os.path.join("PyBank", "Analysis", "Results.txt")
+
+with open(output_path, 'w') as txt_file:
+    txt_file.write(f"""
+    Financial Analysis
+    -----------------    
+    Total Months: {str(month_count)}
+    Total Profit: ${str(profits_total)}
+    Average Change: ${str(round(average_profit))}
+    Greatest Profit Increase: {(months[change_in_profit.index(max(change_in_profit))+1])} ${str(max_change)}
+    Greatest Profit Decrease: {str(months[change_in_profit.index(min(change_in_profit))+1])} ${str(min_change)}
+    """)
